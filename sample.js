4165114ee83263d65e14aed15aae175d9f369b81
techieharry007 <110881478+techieharry007@users.noreply.github.com>
2022-12-22 18:53:37 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+export default function Sample() {
+    const[user,setUser]=useState({name:''})
+    React.useEffect(()=>{
+
+    },[])
+
+    const handleToken= async ()=>{
+      let flag=await localStorage.getItem('tokenKey')
+      if(!!flag)
+      {
+        fetch("http://localhost:8080/api",{
+          method:'GET',
+        }).then((obj)=>{
+          return obj.json()
+        }).then((obj)=>{
+          console.log(obj)
+          console.log('other api triggered')
+        })
+      }
+    }
+
+    const getData=(val)=>{
+      console.log(val)
+      // let formdata=new FormData()
+      // formdata.append('user',val)
+         fetch("http://localhost:8080/api/post",{
+            method:"POST",
+            headers: {
+              'Content-Type': 'application/json'
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify(val)
+        }).then((res)=>{
+          
+        return res.json()
+        // console.log(finalresponse)
+        }).then((res)=>{
+          console.log(res)
+          localStorage.setItem('tokenKey',res.tokenkey)
+        })
+        handleToken()
+        // let finalresponse= result.json()
+        // console.log(finalresponse)
+        // localStorage.setItem('token_key',token_key)
+        // console.log(finalresponse)
+    } 
+    
+  return (
+    <div>
+      <div>Testing</div>
+      <input value={user.name} onChange={(e)=>setUser({...user,name:e.target.value})}/>
+      <button onClick={()=>getData(user)}>Click</button>
+    </div>
+  )
+}
---
+++ server.js
@@ -0,0 +1,61 @@
+var express=require('express')
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const cors=require('cors')
+dotenv.config();
+const bodyParser=require('body-parser')
+process.env.TOKEN_KEY;
+var app=express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+var corsOptions = {
+    origin: 'http://localhost:3001',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+app.use(cors(corsOptions))
+
+function generateAccessToken(username) {
+    return jwt.sign(username, process.env.TOKEN_KEY, { expiresIn: '1800s' });
+  }
+app.post('/api/post',async (req,res)=>{
+    // console.log(req.body,"body data")
+    // let username=req.body.user
+    // console.log(username,"found")
+    const token =  await generateAccessToken({ user: req.body.name });
+    req.body.name.includes('harry')?res.status(200).json({tokenkey:token}):res.status(200).json({msg:"No match found"})
+    
+    // let result=await req.body.name
+    // try{
+    //     if(result.includes('harry'))   
+    //     {
+    //         const token =  generateAccessToken({ user: req.body.user });
+    //         res.json(token);
+    //         res.status(200).json({token_key:token})
+    //         console.log(username)
+    //     }
+    //     else{
+    //         res.status(400).json({msg:"user not found"})
+    //     }
+       
+    // }   
+    // catch(err){
+    //     res.send(400).json({msg:"not found"})
+    // }
+   
+})
+
+app.get('/api', (req, res)=>{
+    res.status(200).json({msg:"successfully implemented JWT"})
+    // jwt.verify(token, process.env.TOKEN_KEY, function(err, decoded){
+    //   if(!err){
+    //     var secrets = {'accountNumber' : '938291239','pin' : '11289','account' : 'Finance'};
+    //     res.json(secrets);
+    //   } else {
+    //     res.send(err,"Error found");
+    //   }
+    // })
+  })
+
+app.get('/api/data',)
+app.listen(8080)
